4766b15d1a646be802480c754c12e7fa1e7504dd
Peter Cordero <petercordero29@gmail.com>
2023-08-14 09:49:03 -0400

    default image

--- models/Portfolio.js
+++ models/Portfolio.js
@@ -4,7 +4,7 @@ const portfolioSchema = new Schema(
     {
         owner: { type: Schema.Types.ObjectId, ref: 'User' },
         title: String,
-        image: String,
+        image: { type: String, default: 'https://media.istockphoto.com/id/1216251206/vector/no-image-available-icon.jpg?s=170667a&w=0&k=20&c=N-XIIeLlhUpm2ZO2uGls-pcVsZ2FTwTxZepwZe4DuE4=' },
         projects: [{ type: Schema.Types.ObjectId, ref: 'Project' }],
     },
     {
--- models/Project.js
+++ models/Project.js
@@ -6,7 +6,7 @@ const projectSchema = new Schema(
         title: String,
         link: String,
         description: String,
-        image: String
+        image: { type: String, default: 'https://media.istockphoto.com/id/1216251206/vector/no-image-available-icon.jpg?s=170667a&w=0&k=20&c=N-XIIeLlhUpm2ZO2uGls-pcVsZ2FTwTxZepwZe4DuE4=' },
     },
     {
         timeseries: true
